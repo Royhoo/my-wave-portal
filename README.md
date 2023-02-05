f817fcff367346876b23ce08b1235642ef9bad7f
royhoo <cnroyhoo@gmail.com>
2023-02-05 16:26:03 +0800

    done

---
+++ README.md
@@ -0,0 +1,24 @@
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
+
+https://goerli.etherscan.io/address/0xc364F19e67EBA775910b4a2560E7624890cA19D1
+
+
+https://replit.com/
+
+
+npx hardhat run scripts/run.js
+
+Deploy again using npx hardhat  run scripts/deploy.js --network goerli
+WavePortal address:  0x30730409257C0Aa356E195E1d5862c2eCF0D9bcd
---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.001"),
+  });
+
+  await waveContract.deployed();
+
+  console.log("WavePortal address: ", waveContract.address);
+
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    "Contract balance:",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
